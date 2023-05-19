533351bd85cea6ec8923fb864412c2af77e9b962
Manas Ranjan Bariha <manasbariha779@gmail.com>
2023-05-19 20:33:14 +0530

    finished with this

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-export default function Square() {
-    const [value,setValue] =useState(null);
-    function handleClick(){
-        setValue('X');
-    }
-    return <button className="square" onClick={handleClick}>{value}</button>;
+export default function Square({value,onSquareClicked}) {
+    
+    return <button className="square" onClick={onSquareClicked}>{value}</button>;
 }
+
+// function Square({ value, onSquareClick }) {
+//     return (
+//       <button className="square" onClick={onSquareClick}>
+//         {value}
+//       </button>
+//     );
+//   }
